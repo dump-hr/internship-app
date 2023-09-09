4f25d4292a80dc558761bf2262e9840c7163949f
Josipa Saravanja <josipa.saravanja06@gmail.com>
2023-09-09 02:47:18 +0200

    code formatting

--- apps/web/src/components/IntervieweeInfo/IntervieweeInfo.tsx
+++ apps/web/src/components/IntervieweeInfo/IntervieweeInfo.tsx
@@ -1,97 +1,109 @@
-import React, { useCallback } from "react";
-import Webcam from "react-webcam";
-import IconButton from "@mui/material/IconButton";
-import PhotoCamera from "@mui/icons-material/PhotoCamera";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useRef, useState } from "react";
-import styles from "../IntervieweeInfo/index.module.css";
+import { useCallback } from 'react';
+import Webcam from 'react-webcam';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useRef, useState } from 'react';
+import styles from '../IntervieweeInfo/index.module.css';
 
 type Info = {
-    fullName: string;
-    email: string;
-    phone: string;
-    dateOfBirth: string;
-    workingStatus: "Učenik" | "Student" | "Nezaposlen" | "Zaposlen";
-    institutionName: string;
-    yearOfStudy: number;
-    field: "Dev" | "Design" | "Marketing" | "Multimedija";
-    referral: "Društvenih mreža" | "Predstavljanja na fakultetima/školama" | "Medija" | "Prijatelja ili poznanika" | "Ostalo"
-    applicationMotivation: string
-}
+  fullName: string;
+  email: string;
+  phone: string;
+  dateOfBirth: string;
+  workingStatus: 'Učenik' | 'Student' | 'Nezaposlen' | 'Zaposlen';
+  institutionName: string;
+  yearOfStudy: number;
+  field: 'Dev' | 'Design' | 'Marketing' | 'Multimedija';
+  referral:
+    | 'Društvenih mreža'
+    | 'Predstavljanja na fakultetima/školama'
+    | 'Medija'
+    | 'Prijatelja ili poznanika'
+    | 'Ostalo';
+  applicationMotivation: string;
+};
 
 type IntervieweeInfoProps = {
-    setUrl: (image: string) => void,
-    info: Info
-}
+  setUrl: (image: string) => void;
+  info: Info;
+};
 
 const IntervieweeInfo = ({ setUrl, info }: IntervieweeInfoProps) => {
-    const videoConstraints = {
-        width: 650,
-        height: 365,
-        aspectRatio: 16 / 9,
+  const videoConstraints = {
+    width: 650,
+    height: 365,
+    aspectRatio: 16 / 9,
+  };
+
+  const webRef = useRef<Webcam>(null);
+  const [image, setImage] = useState<string | undefined>(undefined);
+
+  const capturePicture = useCallback(() => {
+    const imageSrc = webRef.current?.getScreenshot();
+    if (imageSrc) {
+      setImage(imageSrc);
+      setUrl(imageSrc);
     }
+  }, [webRef]);
 
-    const webRef = useRef<Webcam>(null);
-    const [image, setImage] = useState<string | undefined>(undefined);
+  const deletePicture = useCallback(() => {
+    setImage('');
+    setUrl('');
+  }, [webRef]);
 
-    const capturePicture = useCallback(() => {
-        const imageSrc = webRef.current?.getScreenshot();
-        if (imageSrc) {
-            setImage(imageSrc);
-            setUrl(imageSrc)
-        }
-    }, [webRef]);
-
-    const deletePicture = useCallback(() => {
-        setImage("");
-        setUrl("")
-    }, [webRef]);
-
-    return (
-        <div className={styles.container}>
-
-            <div className={styles.interviewInfo}>
-
-                <h1>{info.fullName}</h1>
-                <div className={styles.interviewInfoRow}>
-                    <div className={styles.interviewInfoCol}>
-                        <div>Email: {info.email}</div>
-                        <div>Mobitel: {info.phone} </div>
-                        <div>Datum rođenja: {info.dateOfBirth}</div>
-                        <div>{info.workingStatus}, {info.institutionName}, {info.yearOfStudy}</div>
-                    </div>
-
-                    <div className={styles.interviewInfoCol}>
-                        <div>Područje: {info.field}
-                        </div>
-                        <div>Kako si saznao/la za internship: {info.referral}</div>
-                        <div>Zašto se prijavljujes na internships: {info.applicationMotivation}</div>
-                    </div>
-                </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.interviewInfo}>
+        <h1>{info.fullName}</h1>
+        <div className={styles.interviewInfoRow}>
+          <div className={styles.interviewInfoCol}>
+            <div>Email: {info.email}</div>
+            <div>Mobitel: {info.phone} </div>
+            <div>Datum rođenja: {info.dateOfBirth}</div>
+            <div>
+              {info.workingStatus}, {info.institutionName}, {info.yearOfStudy}
             </div>
+          </div>
 
-            <Webcam
-                style={{ display: image ? "none" : "flex" }}
-                width={650}
-                height={365}
-                audio={false}
-                screenshotFormat="image/jpeg"
-                videoConstraints={videoConstraints}
-                ref={webRef}
-            />
-
-            <img src={image} style={{ display: image ? "flex" : "none" }} />
-            <IconButton style={{
-                color: "#1976d2",
-                position: "absolute",
-                bottom: "0",
-                marginLeft: "auto"
-            }}
-                onClick={() => { image ? deletePicture() : capturePicture() }} aria-label="delete" size="large">
-                {image ? <DeleteIcon /> : <PhotoCamera />}
-            </IconButton>
+          <div className={styles.interviewInfoCol}>
+            <div>Područje: {info.field}</div>
+            <div>Kako si saznao/la za internship: {info.referral}</div>
+            <div>
+              Zašto se prijavljujes na internships: {info.applicationMotivation}
+            </div>
+          </div>
         </div>
-    );
+      </div>
+
+      <Webcam
+        style={{ display: image ? 'none' : 'flex' }}
+        width={650}
+        height={365}
+        audio={false}
+        screenshotFormat="image/jpeg"
+        videoConstraints={videoConstraints}
+        ref={webRef}
+      />
+
+      <img src={image} style={{ display: image ? 'flex' : 'none' }} />
+      <IconButton
+        style={{
+          color: '#1976d2',
+          position: 'absolute',
+          bottom: '0',
+          marginLeft: 'auto',
+        }}
+        onClick={() => {
+          image ? deletePicture() : capturePicture();
+        }}
+        aria-label="delete"
+        size="large"
+      >
+        {image ? <DeleteIcon /> : <PhotoCamera />}
+      </IconButton>
+    </div>
+  );
 };
 
 export default IntervieweeInfo;
--- apps/web/src/pages/InterviewPage/InterviewPage.tsx
+++ apps/web/src/pages/InterviewPage/InterviewPage.tsx
@@ -8,7 +8,6 @@ import { defaultInterviewValues, interviewQuestions, steps } from './data';
 import InterviewQuestionHandler from './InterviewQuestionHandler';
 
 import IntervieweeInfo from '../../components/IntervieweeInfo';
-import { useState } from 'react';
 
 const mapAnswersToQuestions = (answers: FieldValues) => {
   return interviewQuestions.map((q) => ({ ...q, ...answers[q.id] }));
@@ -27,25 +26,26 @@ const InterviewPage = () => {
   );
 
   const setUrl = (image: string) => {
-    console.log(image)
-  }
+    console.log(image);
+  };
 
   return (
     <Box maxWidth="1280px" margin="auto">
       <IntervieweeInfo
         setUrl={setUrl}
         info={{
-          fullName: "Ime i prezime",
-          email: "intern@dump.hr",
-          phone: "091 123 4567",
-          dateOfBirth: "01/01/2020",
-          workingStatus: "Student",
-          institutionName: "Fakultet elektrotehnike i računarstva",
+          fullName: 'Ime i prezime',
+          email: 'intern@dump.hr',
+          phone: '091 123 4567',
+          dateOfBirth: '01/01/2020',
+          workingStatus: 'Student',
+          institutionName: 'Fakultet elektrotehnike i računarstva',
           yearOfStudy: 3,
-          field: "Dev",
-          referral: "Ostalo",
-          applicationMotivation: "Lorem ipsum dolor sit amet, consectetur."
-        }} />
+          field: 'Dev',
+          referral: 'Ostalo',
+          applicationMotivation: 'Lorem ipsum dolor sit amet, consectetur.',
+        }}
+      />
 
       <MultistepForm
         questions={interviewQuestions}
