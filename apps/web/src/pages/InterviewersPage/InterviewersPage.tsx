b67999e1d9c7050e01a0f3f1d9c39803a58a0f74
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-09-23 20:19:02 +0200

    added datagrid to page

--- apps/web/src/pages/InterviewersPage/InterviewersPage.tsx
+++ apps/web/src/pages/InterviewersPage/InterviewersPage.tsx
@@ -1,12 +1,30 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+import { useFetchAllInterviewers } from '../../api/useFetchAllInterviewers';
 import LayoutSpacing from '../../components/LayoutSpacing/LayoutSpacing';
 import LogoHeader from '../../components/LogoHeader';
 
 const InterviewersPage = () => {
+  const { data: interviewers } = useFetchAllInterviewers();
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Ime i prezime', width: 130 },
+    { field: 'disciplines', headerName: 'Područja', width: 130 },
+  ];
+  const rows = [
+    { id: 1, name: 'John Doe', disciplines: 'Matematika' },
+    { id: 2, name: 'Jane Doe', disciplines: 'Matematika' },
+    { id: 3, name: 'John Doe', disciplines: 'Matematika' },
+  ];
   return (
     <>
       <LogoHeader text="Intervjueri" />
       <LayoutSpacing>
-        <p>Content</p>
+        <button onClick={() => console.log(interviewers)}>
+          Log interviewers
+        </button>
+        <DataGrid columns={columns} rows={rows} />
       </LayoutSpacing>
     </>
   );
