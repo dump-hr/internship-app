4554ffe9a0f9f679c0e6396f38cb961de5949f10
drmodun <jan.modun.st@gmail.com>
2023-09-28 06:07:04 +0200

    added x button

--- apps/web/src/pages/EmailPage/EmailPage.tsx
+++ apps/web/src/pages/EmailPage/EmailPage.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Modal } from '@mui/joy';
 
 import { useMakeEmails } from '../../api/useCreateEmails';
+import xSymbol from '../../assets/x-symbol.svg';
 import EmailBox from '../../components/EmailBox';
 import EmailGuide from '../../components/EmailGuide';
 import EmailList from '../../components/EmailList';
@@ -33,23 +34,35 @@ export const EmailPage = ({ emails, on, close }: Props) => {
         justifyContent="space-around"
         padding={'1rem'}
       >
+        <Button
+          sx={{
+            width: '50px',
+            height: '50px',
+            alignSelf: 'flex-end',
+            backgroundColor: 'transparent',
+            color: 'white',
+            marginTop: '0.3rem',
+            transition: 'color 0.3s ease',
+            '&:hover': {
+              backgroundColor: 'transparent',
+              color: 'red',
+              img: { filter: 'invert(1)' },
+            },
+            img: {
+              filter: 'invert(0)',
+              transition: 'filter 0.3s ease',
+              height: '100%',
+              width: '100%',
+            },
+          }}
+          onClick={close}
+        >
+          <img src={xSymbol} alt="close" />
+        </Button>
         Emails
         <EmailBox sendEmail={sendEmails} />
         <EmailGuide />
         <EmailList emails={emails} />
-        <Button
-          sx={{
-            width: '200px',
-            backgroundColor: 'red',
-            color: 'white',
-            marginTop: '1rem',
-            transition: 'background-color 0.3s ease, color 0.3s ease',
-            '&:hover': { backgroundColor: 'gray', color: 'red' },
-          }}
-          onClick={close}
-        >
-          Close
-        </Button>
       </Box>
     </Modal>
   );
