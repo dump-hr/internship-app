83bc56e9c731b468607fc12cf4c99eba99b036e0
dbaric99 <damjana.baric1999@gmail.com>
2023-10-01 19:39:30 +0200

    Removed Internship from logo because it does not follow design

--- apps/web/src/components/Logo/Logo.tsx
+++ apps/web/src/components/Logo/Logo.tsx
@@ -6,7 +6,6 @@ export const Logo = () => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px', width: 200 }}>
       <img src={dumpLogo} height={24} style={{ marginTop: '1px' }} />
-      <Typography sx={{ fontSize: '24px' }}>Internship</Typography>
     </Box>
   );
 };
