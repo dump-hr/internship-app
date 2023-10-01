b8948f50a7b92aa8bf876aee2bc1560ac6ade55f
drmodun <jan.modun.st@gmail.com>
2023-10-01 12:18:36 +0200

    added redirecting with timeout to the homepage

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -1,5 +1,21 @@
 import { Typography } from '@mui/material';
+import { useEffect } from 'react';
 
 export const HomePage = () => {
-  return <Typography>internship-app</Typography>;
+  useEffect(() => {
+    const redirect = setTimeout(() => {
+      window.location.href = 'https://dump.hr/internship';
+    }, 3000);
+
+    return () => {
+      clearTimeout(redirect);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Typography>internship-app</Typography>
+      <Typography>Redircting to dump.hr/internship</Typography>
+    </div>
+  );
 };
