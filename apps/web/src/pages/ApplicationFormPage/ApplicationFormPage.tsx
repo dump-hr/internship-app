52663665bf55d1bd73c8a41633edcb29dc5af542
dbaric99 <damjana.baric1999@gmail.com>
2023-10-01 23:20:53 +0200

    Added font-faces

--- apps/web/src/pages/ApplicationFormPage/ApplicationFormPage.tsx
+++ apps/web/src/pages/ApplicationFormPage/ApplicationFormPage.tsx
@@ -110,136 +110,139 @@ export const ApplicationFormPage = () => {
           alt="form-welcome-image"
         />
       </div>
-      <div></div>
-
-      <form
-        className={classes.formClass}
-        onSubmit={handleSubmit(onSubmit)}
-        noValidate
-      >
-        <div className={classes.formQuestionWrapper}>
-          <label className={classes.marginBottom30px} htmlFor="firstName">
-            Ime:
-          </label>
-          <Input
-            placeholder="Your answer"
-            type="text"
-            id="firstName"
-            {...register('firstName', {
-              required: 'First name is required',
-            })}
-          />
-
-          {errors.firstName ? (
-            <p className={classes.warningText}>{errors.firstName?.message}</p>
-          ) : (
-            <div className={classes.warningTextPlaceholder}></div>
-          )}
+      <div className={classes.applicationForm}>
+        <h2>Prijava na DUMP Internship</h2>
+        <div>
+          Popunite ovu formu s vašim osobnim podacima. Postupak može potrajati
+          nekoliko minuta.
         </div>
-        <div className={classes.formQuestionWrapper}>
-          <label className={classes.marginBottom30px} htmlFor="lastName">
-            Prezime:
-          </label>
-          <Input
-            placeholder="Your answer"
-            type="text"
-            id="lastName"
-            {...register('lastName', {
-              required: 'Last name is required',
-            })}
-          />
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <div className={classes.formQuestionWrapper}>
+            <label className={classes.marginBottom30px} htmlFor="firstName">
+              Ime:
+            </label>
+            <Input
+              placeholder="Your answer"
+              type="text"
+              id="firstName"
+              {...register('firstName', {
+                required: 'First name is required',
+              })}
+            />
 
-          {errors.lastName ? (
-            <p className={classes.warningText}>{errors.lastName?.message}</p>
-          ) : (
-            <div className={classes.warningTextPlaceholder}></div>
-          )}
-        </div>
-        <div className={classes.formQuestionWrapper}>
-          <label className={classes.marginBottom30px} htmlFor="email">
-            Email
-          </label>
-          <Input
-            placeholder="Your answer"
-            type="email"
-            id="email"
-            {...register('email', {
-              required: 'Email is required',
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Invalid email address',
-              },
-            })}
-          />
-
-          {errors.email ? (
-            <p className={classes.warningText}>{errors.email?.message}</p>
-          ) : (
-            <div className={classes.warningTextPlaceholder}></div>
-          )}
-        </div>
-        <div
-          className={clsx(
-            classes.formQuestionWrapper,
-            classes.disciplinesChoiceWrapper,
-          )}
-        >
-          <div className={classes.marginBottom30px}>
-            <label>Prijavljuješ se na:</label>
-            <p className={classes.formQuestionSubtitleText}>
-              Broj područja na koja se možeš prijaviti nije ograničen.
-            </p>
+            {errors.firstName ? (
+              <p className={classes.warningText}>{errors.firstName?.message}</p>
+            ) : (
+              <div className={classes.warningTextPlaceholder}></div>
+            )}
           </div>
+          <div className={classes.formQuestionWrapper}>
+            <label className={classes.marginBottom30px} htmlFor="lastName">
+              Prezime:
+            </label>
+            <Input
+              placeholder="Your answer"
+              type="text"
+              id="lastName"
+              {...register('lastName', {
+                required: 'Last name is required',
+              })}
+            />
 
-          {disciplineEnumKeys.map((discipline, index) => (
-            <div key={discipline}>
-              <Checkbox
-                value={disciplineEnumValues[index]}
-                label={disciplineLabel[disciplineEnumValues[index]]}
-                {...register('disciplines', {
-                  required: 'This field is required',
-                })}
-                onChange={(e) => handleCheckboxChange(e)}
-                name="disciplines"
-                checked={internDisciplines.includes(
-                  disciplineEnumValues[index],
-                )}
-              />
-            </div>
-          ))}
+            {errors.lastName ? (
+              <p className={classes.warningText}>{errors.lastName?.message}</p>
+            ) : (
+              <div className={classes.warningTextPlaceholder}></div>
+            )}
+          </div>
+          <div className={classes.formQuestionWrapper}>
+            <label className={classes.marginBottom30px} htmlFor="email">
+              Email
+            </label>
+            <Input
+              placeholder="Your answer"
+              type="email"
+              id="email"
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Invalid email address',
+                },
+              })}
+            />
 
-          {errors.disciplines ? (
-            <p className={classes.warningText}>{errors.disciplines?.message}</p>
-          ) : (
-            <div className={classes.warningTextPlaceholder}></div>
-          )}
-        </div>
-        {internDisciplines.length > 1 && (
-          <SortableDisciplinesContainer
-            internDisciplines={internDisciplines}
-            setInternDisciplines={setInternDisciplines}
-          />
-        )}
-
-        {applicationFormDataQuestions.map((question) => (
-          <ApplicationFormInputHandler
-            key={question.id}
-            question={question}
-            register={register}
-            errors={errors}
-            watch={watch}
-          />
-        ))}
-        <div className={classes.formActionsWrapper}>
-          <Button
-            type="submit"
-            variant="contained"
-            className={classes.submitButton}
+            {errors.email ? (
+              <p className={classes.warningText}>{errors.email?.message}</p>
+            ) : (
+              <div className={classes.warningTextPlaceholder}></div>
+            )}
+          </div>
+          <div
+            className={clsx(
+              classes.formQuestionWrapper,
+              classes.disciplinesChoiceWrapper,
+            )}
           >
-            Submit
-          </Button>
-        </div>
-      </form>
+            <div className={classes.marginBottom30px}>
+              <label>Prijavljuješ se na:</label>
+              <p className={classes.formQuestionSubtitleText}>
+                Broj područja na koja se možeš prijaviti nije ograničen.
+              </p>
+            </div>
+
+            {disciplineEnumKeys.map((discipline, index) => (
+              <div key={discipline}>
+                <Checkbox
+                  value={disciplineEnumValues[index]}
+                  label={disciplineLabel[disciplineEnumValues[index]]}
+                  {...register('disciplines', {
+                    required: 'This field is required',
+                  })}
+                  onChange={(e) => handleCheckboxChange(e)}
+                  name="disciplines"
+                  checked={internDisciplines.includes(
+                    disciplineEnumValues[index],
+                  )}
+                />
+              </div>
+            ))}
+
+            {errors.disciplines ? (
+              <p className={classes.warningText}>
+                {errors.disciplines?.message}
+              </p>
+            ) : (
+              <div className={classes.warningTextPlaceholder}></div>
+            )}
+          </div>
+          {internDisciplines.length > 1 && (
+            <SortableDisciplinesContainer
+              internDisciplines={internDisciplines}
+              setInternDisciplines={setInternDisciplines}
+            />
+          )}
+
+          {applicationFormDataQuestions.map((question) => (
+            <ApplicationFormInputHandler
+              key={question.id}
+              question={question}
+              register={register}
+              errors={errors}
+              watch={watch}
+            />
+          ))}
+          <div className={classes.formActionsWrapper}>
+            <Button
+              type="submit"
+              variant="contained"
+              className={classes.submitButton}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
