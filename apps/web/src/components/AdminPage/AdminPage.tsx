9e2d30cca45628c78256440eb54160ed1317d9f7
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-10-02 21:45:12 +0200

    updated AdminPage

--- apps/web/src/components/AdminPage/AdminPage.tsx
+++ apps/web/src/components/AdminPage/AdminPage.tsx
@@ -1,42 +1,17 @@
-import { Box, Button } from '@mui/material';
-import { toast } from 'react-hot-toast';
-import { Link } from 'wouter';
-import { navigate } from 'wouter/use-location';
-
-import { Path } from '../../constants/paths';
-import { Logo } from '../Logo';
+import LayoutSpacing from '../LayoutSpacing/LayoutSpacing';
+import LogoHeader from '../LogoHeader';
 
 type Props = {
+  headerText?: string;
   children?: React.ReactNode;
 };
 
-const AdminPage: React.FC<Props> = ({ children }) => {
-  const handleLogout = () => {
-    toast.success('Logged out');
-    localStorage.removeItem('access_token');
-    navigate(Path.Login);
-  };
-
+const AdminPage: React.FC<Props> = ({ headerText, children }) => {
   return (
-    <Box maxWidth="1280px" margin="auto" padding="20px">
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          marginBottom: '20px',
-        }}
-      >
-        <Logo />
-        <Box sx={{ display: 'flex', gap: '12px' }}>
-          <Button component={Link} to={Path.Dashboard}>
-            Dashboard
-          </Button>
-          <Button onClick={handleLogout}>Logout</Button>
-        </Box>
-      </Box>
-      {children}
-    </Box>
+    <>
+      <LogoHeader text={headerText || ''} />
+      <LayoutSpacing>{children}</LayoutSpacing>
+    </>
   );
 };
 
