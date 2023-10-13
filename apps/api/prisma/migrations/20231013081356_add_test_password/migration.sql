bd05492d325f985553b392b7c146611f2a5b2fd5
fdoljanin <doljaninfrane@gmail.com>
2023-10-13 10:18:55 +0200

    Migration default fix

--- apps/api/prisma/migrations/20231013081356_add_test_password/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The required column `password` was added to the `TestSlot` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
-
-*/
--- AlterTable
-ALTER TABLE "TestSlot" ADD COLUMN     "password" TEXT NOT NULL;
