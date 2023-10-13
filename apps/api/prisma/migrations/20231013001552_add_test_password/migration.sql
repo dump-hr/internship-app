a0a2acc068100cd79df706a28f7bf937c9997de8
fdoljanin <doljaninfrane@gmail.com>
2023-10-13 10:14:36 +0200

    Password default set

--- apps/api/prisma/migrations/20231013001552_add_test_password/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `password` to the `TestSlot` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TestSlot" ADD COLUMN     "password" TEXT NOT NULL;
