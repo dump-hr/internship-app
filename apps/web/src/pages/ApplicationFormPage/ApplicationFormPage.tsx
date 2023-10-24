6dec257226041069dc8aee8afb37122d53fa3abc
Bartol Deak <b@bdeak.net>
2023-10-24 19:26:42 +0200

    open applcations again temporary

--- apps/web/src/pages/ApplicationFormPage/ApplicationFormPage.tsx
+++ apps/web/src/pages/ApplicationFormPage/ApplicationFormPage.tsx
@@ -35,7 +35,7 @@ export type FormValues = {
 const disciplineEnumKeys = Object.keys(Discipline);
 const disciplineEnumValues = Object.values(Discipline);
 
-const applicationsClosed = true;
+const applicationsClosed = false;
 
 export const ApplicationFormPage = () => {
   const [internDisciplines, setInternDisciplines] = useState<Discipline[]>([]);
