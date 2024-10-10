d6df419a171e9b09b607a35d18fce5e782160f9d
Lucija Fradelić <lucija@Lucijas-MacBook-Air.local>
2024-10-10 15:53:21 +0200

    switched to kebab case

--- apps/api/src/intern/intern.service.ts
+++ apps/api/src/intern/intern.service.ts
@@ -218,12 +218,12 @@ export class InternService {
     const marketingFormAdditionalText = `U nastavku se nalazi link na formu, za područje Marketing, koju je obavezno ispuniti prije samog intervjua za DUMP Internship. 
     Za ispunjavanje je predviđeno 15ak minuta, ali uzmi vremena koliko god ti treba. 
 
-    Link: https://bit.ly/marketingForma`;
+    Link: https://bit.ly/marketing-forma`;
 
     const devFormAdditionalText = `U nastavku se nalazi link na primjer prošlogodišnjeg ispita za smjer programiranja na DUMP Internshipu.
     Zadatke možeš rješavati u jednom od sljedećih jezika: JavaScript, Python, C#, C++, C, Java, Go, a za rješavanje je predviđeno 90 minuta.
 
-    Link: https://bit.ly/PrimjerInicijalnog`;
+    Link: https://bit.ly/primjer-inicijalnog`;
 
     const generalTextEnding = `Lijep pozdrav,
     
