179b886a8ebcdc88c327e651a247afd1518a710b
ninom5 <nmilas00@fesb.hr>
2025-08-29 23:00:37 +0200

    change interview email

--- apps/api/src/interview-slot/interview-slot.service.ts
+++ apps/api/src/interview-slot/interview-slot.service.ts
@@ -268,7 +268,7 @@ export class InterviewSlotService {
     biranje termina intervjua je uspješno provedeno! Termin svog intervjua možeš vidjeti na status stranici: https://internship.dump.hr/status/${intern.id}
     U slučaju da ipak ne možeš doći na odabrani termin, javi nam se na vrijeme na info@dump.hr
     
-    Podsjećamo, tvoj intervju će se održati u odabranom terminu u našem uredu (prostorija A223) na FESB-u (Ruđera Boškovića 32).
+    Podsjećamo, tvoj intervju za sva područja će se održati u odabranom terminu u našem uredu (prostorija A223) na FESB-u (Ruđera Boškovića 32).
     
     Naš ured ćeš pronaći tako da kad uđeš kroz glavna vrata FESB-a skreneš desno do kraja hodnika (put referade) dok ne dođeš do stepenica koje su s lijeve strane. Popneš se stepenicama na prvi kat i skreneš lijevo. Nastaviš hodnikom do kraja i s desne strane vidjet ćeš vrata našeg ureda (A223).
     
